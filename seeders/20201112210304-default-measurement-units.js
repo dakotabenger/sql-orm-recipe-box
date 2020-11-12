6190d749bf0594ac184cc9662e6306a6b4f2af94
Dakota Benger <dakota@bengerlaw.com>
2020-11-12 14:09:19 -0700

    Seed and migrates

---
+++ seeders/20201112210304-default-measurement-units.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+   return queryInterface.bulkInsert('MeasurementUnits', [
+    { name: 'cups', createdAt: new Date(), updatedAt: new Date() },
+  ]);
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
